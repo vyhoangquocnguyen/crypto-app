808f52c5d10c4d51ce4744285d7e6dcd54a82e20
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2026-01-06 17:26:39 +0700

    navigation

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href="/">
+          <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+        </Link>
+        <nav>
+          <Link
+            href="/"
+            className={cn("nav-link", {
+              "is-active": pathname === "/",
+              "is-home": true,
+            })}>
+            Home
+          </Link>
+          {/* Search Modal */}
+          <Link
+            href="/coins"
+            className={cn("nav-link", {
+              "is-active": pathname === "/coins",
+            })}>
+            All Coins
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
