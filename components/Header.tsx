fd1dc73f96be4d7e5bc917a4107a79c5ecce23a7
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2026-01-16 01:28:45 +0700

    fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { Search } from "lucide-react";
+
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -24,7 +24,7 @@ export default function Header() {
             Home
           </Link>
           {/* Search Modal */}
-          <SearchModal />
+          <SearchModal initialTrendingCoins={[]} />
           <Link
             href="/coins"
             className={cn("nav-link", {
