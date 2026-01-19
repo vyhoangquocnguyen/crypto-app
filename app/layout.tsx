44c1470c8403d5b878efbf6181bba19d2a7ee73b
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2026-01-19 18:01:46 +0700

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Crypto App",
-  description: "Cryopto Screener  App with a built-in High-Frequency Terminal & Dashboard",
+  description: "Crypto Screener  App with a built-in High-Frequency Terminal & Dashboard",
 };
 
 export default function RootLayout({
